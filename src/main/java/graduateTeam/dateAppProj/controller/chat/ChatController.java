758b14bec0ac7565c6cf33b8ed26244ea8596aa0
dyparkkk <dkdlel1z@naver.com>
2021-10-21 15:36:39 +0900

    message

--- src/main/java/graduateTeam/dateAppProj/controller/chat/ChatController.java
+++ src/main/java/graduateTeam/dateAppProj/controller/chat/ChatController.java
@@ -4,12 +4,14 @@ import graduateTeam.dateAppProj.controller.chat.dto.ChatMessageRequestDto;
 import graduateTeam.dateAppProj.controller.chat.dto.ChatMessageResponseDto;
 import graduateTeam.dateAppProj.service.ChatService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class ChatController {
 
     private final SimpMessagingTemplate messagingTemplate;
@@ -17,6 +19,7 @@ public class ChatController {
 
     @MessageMapping("/chat/message") //  /pub/chat/message
     public void message(ChatMessageRequestDto messageDto) {
+        log.info("message : " + messageDto.getMessage());
         ChatMessageResponseDto responseDto = chatService.sendMessage(messageDto);
         messagingTemplate.convertAndSend("/sub/chat/room/"+ responseDto.getRoomId(), responseDto);
     }                           //    /sub/chat/room/roomId
