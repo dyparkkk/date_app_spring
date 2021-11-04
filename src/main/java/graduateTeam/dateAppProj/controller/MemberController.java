af894db552fbe7edc444bb857b649173f922ee82
dyparkkk <dkdlel1z@naver.com>
2021-11-04 09:29:47 +0900

    logout

--- src/main/java/graduateTeam/dateAppProj/controller/MemberController.java
+++ src/main/java/graduateTeam/dateAppProj/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -75,5 +76,12 @@ public class MemberController {
         }
 
         return response;
+    }
+
+    @GetMapping("/logout")
+    @ResponseBody
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "logout";
     }
 }
